8be897fa8892a0f772d95d50cb1771287644ee10
Waleed Raza <raanawaleed@gmail.com>
2026-03-24 03:30:04 +0500

    Models + Relations  + migrations

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['project_id', 'name', 'priority'];
+
+    protected $casts = [
+        'priority'   => 'integer',
+        'project_id' => 'integer',
+    ];
+
+    public function user() { return $this->belongsTo(User::class); }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
